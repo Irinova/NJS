571dc88b34c1f678e26eac45571c2236348c5fc4
Irina Zakharova <letter.zakharova@ya.ru>
2024-03-10 16:54:27 +0300

    feat: 005-http

---
+++ 005-http/index.ts
@@ -0,0 +1,24 @@
+const config = require('./config')
+const readline = require('node:readline')
+const { stdin: input, stdout: output } = require('node:process')
+
+const rl = readline.createInterface({ input, output })
+
+rl.question(`Input city: `, (input) => {
+  input = input || 'Moscow'
+  fetch(`http://api.weatherapi.com/v1/current.json?key=${config.API_KEY}&q=${input}&aqi=no`)
+    .then(response => response.json())
+    .then((json) => {
+      const temp = json?.current?.temp_c?.toString() || ''
+      const condition = json?.current?.condition?.text || ''
+      const humidity = json?.current?.humidity?.toString() || ''
+      console.log(`
++----------------------------------+---------+------------------------+----------------+
+|City                              |Temp     |Condition               |Humidity        |
++----------------------------------+---------+------------------------+----------------+
+|${input.padEnd(34, ' ')}|${temp.padEnd(9, ' ')}|${condition.padEnd(24, ' ')}|${humidity.padEnd(16, ' ')}|
++----------------------------------+---------+------------------------+----------------+
+`)
+    })
+  rl.close()
+})
